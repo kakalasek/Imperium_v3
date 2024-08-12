c9e274c064b8c05c97d8451d5fb074e9bc5fde9d
Kakalasek <honziccekkj@seznam.cz>
2024-08-12 11:45:19 +0200

    Error handling and comments

--- controller/Flask_app/src/controller.py
+++ controller/Flask_app/src/controller.py
@@ -25,16 +25,20 @@ endpoints = {   # Dictionary which stores all set and unset endpoints
 
 # Special functions #
 def get_scans():    # Used to retrieve scans from the database
-    global scans
-    scans = []  # Sets scans to an empty array, so the scans wont be added there twice
+    try:
+        # Variables
+        global scans
+        scans = []  # Sets scans to an empty array, so the scans wont be added there twice
 
-    for scan in Scan.query.all():   # Fills the 'scans' array with scans from the database
-            scans.append({
-                'id': scan.id,
-                'name': scan.name,
-                'target': scan.target,
-                'scan_json': scan.scan_json
-            })
+        for scan in Scan.query.all():   # Fills the 'scans' array with scans from the database
+                scans.append({
+                    'id': scan.id,
+                    'name': scan.name,
+                    'target': scan.target,
+                    'scan_json': scan.scan_json
+                })
+    except Exception as e:  # Probably only database error can arise here, so I am calling every error a database error. Without a database, the app cant work, so this shut down the application
+        r = handle_error("DatabaseError", e)
 
 # Routes #
 
@@ -53,6 +57,7 @@ def home():
 @app.route("/scanner", methods=['GET', 'POST']) # This route is the default page for the scanner extension
 def scanner():
     try:
+        # Variables
         test = requests.get(f"{endpoints['scanner']}/@test")    # Check if the scanner node is alive
                                                                 # If not, it throws a ConnectionError, which is handled at the end
 
@@ -106,78 +111,116 @@ def scanner():
         return r
 
 # The scan route
-@app.route("/scanner/scan")
+@app.route("/scanner/scan") # This route is a page for a particular scan
 def scan():
-    global scans
-    scan_id = request.args.get('scan_id')
-    scan_json = {}
-
-    get_scans()
-
-    for entry in scans:
-        if entry["id"] == int(scan_id):
-            scan_json = json.loads(entry["scan_json"])
-            break
-
-    return render_template('scan.html', scan_json=scan_json, scan_id=scan_id)
-
-# The host route
-@app.route("/scanner/host")
-def host():
-    global scans
-    without_mac = True
-    scan_id = request.args.get('scan_id')
-    host_ip = request.args.get('host_ip')
-    host_json = {}
-
-    for entry in scans:
-        if entry["id"] == int(scan_id):
-            scan_json = json.loads(entry["scan_json"])
-            if isinstance(scan_json['host'], dict):
-                if "@addr" in scan_json['host']['address']:
-                    host_json = scan_json['host']
-                else:
-                    host_json = scan_json['host']
-                    without_mac = False
-            else:
-                for host in scan_json['host']:
-                    if "@addr" in host['address']:
-                        if host['address']['@addr'] == host_ip:
-                            host_json = host
-                            break
-                    elif host['address'][0]['@addr'] == host_ip:
-                        host_json = host
-                        without_mac = False
-                        break
-
-            break
-
-    return render_template('host.html', data=host_json, without_mac=without_mac, scan_id =scan_id, host_ip=host_ip)
-
-@app.route("/scanner/scan/show_json")
-def show_json():
+    try:
+        # Variables
         global scans
         scan_id = request.args.get('scan_id')
 
-        if scan_id == None:
-            abort(404)
+        if scan_id == None: # If no scan id is provided, this page raises an exception
+            raise requests.exceptions.RequestException("Invalid Scan ID")
 
-        if request.args.get('host_ip'):
+        scan_json = {}  # JSON for this particular scan will be stored here
+
+        get_scans() # Refresh the scans table
+
+        for entry in scans: # Check for each entry in the scans table. If the scan is found, load it into 'scan_json' and break
+            if entry["id"] == int(scan_id):
+                scan_json = json.loads(entry["scan_json"])
+                break
+        
+        if not scan_json:   # If provided scan id was not found, this page raises an exception
+            raise requests.exceptions.RequestException("Invalid Scan ID")
+
+        return render_template('scan.html', scan_json=scan_json, scan_id=scan_id), 200
+    
+    except requests.exceptions.RequestException as e:
+        r = handle_error('RequestException', e)
+        return r
+    except Exception as e:
+        r = handle_error('', e)
+        return r
+
+# The host route
+@app.route("/scanner/host") # This route is a page for a particular host
+def host():
+    try:
+        # Variables
+        global scans
+        without_mac = True  # Is here because the json looks differently depending on if the scan was able to determine the MAC address or not
+        scan_id = request.args.get('scan_id')
+        host_ip = request.args.get('host_ip')
+        host_json = {}
+
+        if scan_id == None: # If no scan id is provided, this page raises an exception
+            raise requests.exceptions.RequestException("Invalid Scan ID")
+        
+        if host_ip == None: # If no host_ip is provided, this page raises an exception
+            raise requests.exceptions.RequestException("Invalid Host IP")
+
+        for entry in scans: # Looking through scans for scan with the id
+            if entry["id"] == int(scan_id): # There right scan was found
+                scan_json = json.loads(entry["scan_json"])
+                if isinstance(scan_json['host'], dict): # If scan_json['host'] is a dictionary a single host was scanned, so there is no need for further ip control
+                    if "@addr" in scan_json['host']['address']: # Determine if MAC address was found
+                        host_json = scan_json['host']
+                    else:
+                        host_json = scan_json['host']
+                        without_mac = False
+                else:   # Multiple hosts were scanned, so the right one must be found
+                    for host in scan_json['host']:  # Find the right host
+                        if "@addr" in host['address']:  # After finding the host, determine if MAC address was found too
+                            if host['address']['@addr'] == host_ip:
+                                host_json = host
+                                break
+                        elif host['address'][0]['@addr'] == host_ip:
+                            host_json = host
+                            without_mac = False
+                            break
+                break
+
+            if not host_json:   # If provided scan id was not found, this page raises an exception
+                raise requests.exceptions.RequestException("Invalid Scan ID or Host IP")
+
+        return render_template('host.html', data=host_json, without_mac=without_mac, scan_id =scan_id, host_ip=host_ip), 200
+    
+    except requests.exceptions.RequestException as e:
+        r = handle_error('RequestException', e)
+        return r
+    except Exception as e:
+        r = handle_error('', e)
+        return r
+
+# The scan JSON route
+@app.route("/scanner/scan/show_json")   # This route show the raw JSON of a particular scan or host
+def show_json():
+    try:
+        # Variables
+        global scans
+        scan_id = request.args.get('scan_id')
+        host_json = {}
+        scan_json = {}
+
+        if scan_id == None: # If no scan ID was provided, this page raises an exception
+            raise requests.exceptions.RequestException("Invalid Scan ID")
+
+        if request.args.get('host_ip'): # For JSON of a host
             host_ip = request.args.get('host_ip')
 
-            for entry in scans:
-                if entry["id"] == int(scan_id):
+            for entry in scans: # Looking through scans for scan with the id
+                if entry["id"] == int(scan_id): # There right scan was found
                     scan_json = json.loads(entry["scan_json"])
-                    if isinstance(scan_json['host'], dict):
-                        if "@addr" in scan_json['host']['address']:
+                    if isinstance(scan_json['host'], dict): # If scan_json['host'] is a dictionary a single host was scanned, so there is no need for further ip control
+                        if "@addr" in scan_json['host']['address']: # Determine if MAC address was found
                             host_json = scan_json['host']
                             return host_json, 200
                         else:
                             host_json = scan_json['host']
                             return host_json, 200
-                    else:
-                        for host in scan_json['host']:
-                            if "@addr" in host['address']:
+                    else:   # Multiple hosts were scanned, so the right one must be found
+                        for host in scan_json['host']:  # Find the right host
+                            if "@addr" in host['address']:  # After finding the host, determine if MAC address was found too
                                 if host['address']['@addr'] == host_ip:
                                     host_json = host
                                     return host_json, 200
@@ -185,15 +228,28 @@ def show_json():
                                 host_json = host
                                 return host_json, 200
                     break
-                
-        else:
 
-            for entry in scans:
-                if entry["id"] == int(scan_id):
+            if not host_json:   # If an invalid host IP was provided, this page raises an exception
+                raise requests.exceptions.RequestException("Invalid Scan ID or Host IP")
+                
+        else:   # For JSON of a scan
+
+            for entry in scans: # Looking through scans for scan with the id
+                if entry["id"] == int(scan_id): # The right scan was found
                     scan_json = json.loads(entry["scan_json"])
                     return scan_json, 200
+                
+            if not scan_json:   # If the scan ID is invalid, thsi page raises an exception
+                raise requests.exceptions.RequestException("Invalid Scan ID")
+                
+    except requests.exceptions.RequestException as e:
+        r = handle_error('RequestException', e)
+        return r
+    except Exception as e:
+        r = handle_error('', e)
+        return r
 
-        abort(404)            
 
+# The start of the program
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
--- controller/Flask_app/src/errorhandler.py
+++ controller/Flask_app/src/errorhandler.py
@@ -1,5 +1,6 @@
 from flask import render_template
 import logging
+import sys
 
 log_format = '%(asctime)s - %(message)s'
 logging.basicConfig(format=log_format)
@@ -11,6 +12,12 @@ def handle_error(code, e):
         case "EndpointNotSet":
             log.info(f'Endpoint not set\n{e}')
             return render_template('endpoint_not_set.html'), 402
+        case "DatabaseError":
+            log.error(f'Database Error\n{e}')
+            sys.exit()
+        case "RequestException":
+            log.error(f'Request Exception\n{e}')
+            return render_template('404.html'), 400 
         case _:
             log.error(f'Unknown Error\n{e}')
-            return render_template('unkown_exceptino'), 500
+            return render_template('unkown_exception'), 500
